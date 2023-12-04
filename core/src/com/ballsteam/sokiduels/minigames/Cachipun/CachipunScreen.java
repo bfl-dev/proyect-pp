dcc1f6b44a1091de809926386b6511429d6cf57d
Feegz <f.seguel03@ufromail.cl>
2023-12-03 23:26:36 -0300

    cambios main screen

--- core/src/com/ballsteam/sokiduels/minigames/Cachipun/CachipunScreen.java
+++ core/src/com/ballsteam/sokiduels/minigames/Cachipun/CachipunScreen.java
@@ -2,12 +2,14 @@ package com.ballsteam.sokiduels.minigames.Cachipun;
 
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.ballsteam.sokiduels.Screens.AbstractScreen;
 import com.ballsteam.sokiduels.SokiDuels;
+import com.ballsteam.sokiduels.minigames.spaceinvaders.SokiInvadersScreen;
 import com.ballsteam.sokiduels.player.ControllerInput;
 import com.ballsteam.sokiduels.player.KeyboardInput;
 import com.ballsteam.sokiduels.player.Player;
@@ -21,26 +23,25 @@ public class CachipunScreen extends AbstractScreen {
     boolean[] J1_ACTION = new boolean[]{false,false,false,false,false};
     boolean[] J2_ACTION = new boolean[]{false,false,false,false,false};
 
-    Sprite D_PAD_CONTROLLS;
-    Sprite KEYBOARD_CONTROLS;
     Music music_background;
     Sprite fondo;
     Sprite player1Sprite;
     Sprite player2Sprite;
     Player J1;
     Player J2;
+    Texture [] choicheTexture;
+    long timeOut;
     HashMap<Player, boolean[]> players = new HashMap<>();
-    HashMap<Player, Integer> eleccion = new HashMap<>();
+    HashMap<Player, Integer> choice = new HashMap<>();
     public CachipunScreen(SokiDuels main, Player J1, Player J2) {
         super(main);
         players.put(J1,J1_ACTION);
         players.put(J2,J2_ACTION);
-        eleccion.put(J1,0);
-        eleccion.put(J2,0);
+        choice.put(J1,0);
+        choice.put(J2,0);
+        choicheTexture = new Texture[]{new Texture("sword.png"),new Texture("jojo.png"),new Texture("shield.png")};
         player1Sprite = new Sprite(new Texture("soki.png"));
         player2Sprite = new Sprite(new Texture("soki.png"));
-        D_PAD_CONTROLLS = new Sprite(new Texture("ControllerCachipun.png"));
-        KEYBOARD_CONTROLS = new Sprite(new Texture("KeyboardCachipun.png"));
         fondo = new Sprite(new Texture("cachipunBackground.png"));
         music_background = Gdx.audio.newMusic(Gdx.files.internal("background_music.mp3"));
         music_background.setLooping(true);
@@ -54,9 +55,8 @@ public class CachipunScreen extends AbstractScreen {
         player1Sprite.setScale(1.5f);
         music_background.setVolume(0.05f);
         music_background.play();
-        player1Sprite.setPosition(SCREEN_WIDTH/3,SCREEN_HEIGHT-200);
-        player2Sprite.setPosition((SCREEN_WIDTH/3)*2,SCREEN_HEIGHT-200);
-        KEYBOARD_CONTROLS.setPosition(player1Sprite.getX()-175, player1Sprite.getY()-500);
+        player1Sprite.setPosition(SCREEN_WIDTH/3,SCREEN_HEIGHT-300);
+        player2Sprite.setPosition((SCREEN_WIDTH/3)*2,SCREEN_HEIGHT-300);
     }
     @Override
     public void render(float delta) {
@@ -69,32 +69,81 @@ public class CachipunScreen extends AbstractScreen {
         action(J2);
         player1Sprite.draw(main.batch);
         player2Sprite.draw(main.batch);
+        determineWinner(J1,J2);
         J2.draw(main.batch,SCREEN_WIDTH,SCREEN_HEIGHT);
         J1.draw(main.batch,SCREEN_WIDTH,SCREEN_HEIGHT);
-        KEYBOARD_CONTROLS.draw(main.batch);
         main.batch.end();
     }
     public void action(Player player){
-        if(eleccion.get(player)==0) {
+        if(choice.get(player)==0) {
             if (players.get(player)[0]) {
-                KEYBOARD_CONTROLS.setTexture(new Texture("ready.png"));
-
+                player.setPlayerAction(new Texture("ready.png"));
+                choice.replace(player, 1);
             }
             if (players.get(player)[1]) {
-                KEYBOARD_CONTROLS.setTexture(new Texture("ready.png"));
-
+                player.setPlayerAction(new Texture("ready.png"));
+                choice.replace(player, MathUtils.random(1, 3));
             }
             if (players.get(player)[2]) {
-                KEYBOARD_CONTROLS.setTexture(new Texture("ready.png"));
-
+                player.setPlayerAction(new Texture("ready.png"));
+                choice.replace(player, 2);
             }
             if (players.get(player)[3]) {
-                KEYBOARD_CONTROLS.setTexture(new Texture("ready.png"));
-                System.out.println("eleccionPlayer1");
+                player.setPlayerAction(new Texture("ready.png"));
+                choice.replace(player, 3);
             }
-        }else {if (players.get(player)[4])
-            KEYBOARD_CONTROLS.setTexture(new Texture("KeyboardCachipun.png"));
+        }else {if (players.get(player)[4]) {
+            player.setPlayerAction(new Texture("KeyboardCachipun.png"));
+            choice.put(player, 0);
+            }
         }
+    }
+    public void determineWinner(Player J1,Player J2){
+        if (choice.get(J1)!=0 && choice.get(J2)!=0) {
+            if (choice.get(J1) == choice.get(J2)) {
+                J1.setPlayerAction(choicheTexture[choice.get(J1) - 1]);
+                J2.setPlayerAction(choicheTexture[choice.get(J2) - 1]);
+            }
+            if (choice.get(J1) == 1 && choice.get(J2) == 2) {
+                J1.setPlayerAction(choicheTexture[choice.get(J1) - 1]);
+                J2.setPlayerAction(choicheTexture[choice.get(J2) - 1]);
+                    try {
+                        Thread.sleep(10000);
+                        enterGame(J1,J2,new SokiInvadersScreen(main, J1, J2));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    enterGame(J1,J2,new SokiInvadersScreen(main, J1, J2));
+            }
+            if (choice.get(J1) == 1 && choice.get(J2) == 3) {
+                J1.setPlayerAction(choicheTexture[choice.get(J1) - 1]);
+                J2.setPlayerAction(choicheTexture[choice.get(J2) - 1]);
+                //main.setScreen(new sokidefense(main, J2, J1));
+            }
+            if (choice.get(J1) == 2 && choice.get(J2) == 1) {
+                J1.setPlayerAction(choicheTexture[choice.get(J1) - 1]);
+                J2.setPlayerAction(choicheTexture[choice.get(J2) - 1]);
+                enterGame(J2,J1,new SokiInvadersScreen(main, J2, J1));
+            }
+            if (choice.get(J1) == 2 && choice.get(J2) == 3) {
+                J1.setPlayerAction(choicheTexture[choice.get(J1) - 1]);
+                J2.setPlayerAction(choicheTexture[choice.get(J2) - 1]);
+                //main.setScreen(new baile(main, J2, J1));
+            }
+            if (choice.get(J1) == 3 && choice.get(J2) == 1) {
+                J1.setPlayerAction(choicheTexture[choice.get(J1) - 1]);
+                J2.setPlayerAction(choicheTexture[choice.get(J2) - 1]);
+                //main.setScreen(new sokidefense(main, J2, J1));
+            }
+            if (choice.get(J1) == 3 && choice.get(J2) == 2) {
+                J1.setPlayerAction(choicheTexture[choice.get(J1) - 1]);
+                J2.setPlayerAction(choicheTexture[choice.get(J2) - 1]);
+                //main.setScreen(new baile(main, J2, J1));
+            }
+        }
+    }
+    public void enterGame(Player win,Player lose,AbstractScreen screen){
+        main.setScreen(screen);
     }
     private void updatePlayerArrows(Player player){
         player.Input.update();
@@ -115,6 +164,6 @@ public class CachipunScreen extends AbstractScreen {
         players.get(player)[1] = player.Input.DOWN==1;
         players.get(player)[2] = player.Input.UP==1;
         players.get(player)[3] = player.Input.RIGHT==1;
-        players.get(player)[4] = player.Input.B;
+        players.get(player)[4] = player.Input.A;
     }
 }
--- core/src/com/ballsteam/sokiduels/player/Player.java
+++ core/src/com/ballsteam/sokiduels/player/Player.java
@@ -9,6 +9,7 @@ public class Player {
     private final boolean isPlayerOne;
     private int health;
     private Sprite bar;
+    Sprite playerAction;
     public PlayerInput Input = new PlayerInput() {
         @Override
         public void update() {
@@ -19,6 +20,7 @@ public class Player {
         this.isPlayerOne = isPlayerOne;
         this.health = 300;
         this.bar = new Sprite(new Texture("green.png"));
+        playerAction = new Sprite(new Texture("KeyboardCachipun.png"));
     }
     public Player(boolean isPlayerOne, PlayerInput InputMethod){
         this.isPlayerOne = isPlayerOne;
@@ -37,8 +39,10 @@ public class Player {
     public void draw(SpriteBatch batch, float width, float height){
         if (isPlayerOne) {
             bar.setPosition((width/3)-125, (height-100));
+            playerAction.setPosition((width/3)-175, height-700);
         } else {
             bar.setPosition((((width/3)*2)-125), (height-100));
+            playerAction.setPosition(((width/3)*2)-175, height-700);
         }
         if(health < 25) {
             bar.setTexture(new Texture("red.png"));
@@ -46,5 +50,9 @@ public class Player {
             bar.setTexture(new Texture("yellow.png"));
         }
         batch.draw(bar, bar.getX(), bar.getY(), health, 20);
+        batch.draw(playerAction, playerAction.getX(), playerAction.getY());
+    }
+    public void setPlayerAction(Texture texture){
+        this.playerAction.setTexture(texture);
     }
 }
